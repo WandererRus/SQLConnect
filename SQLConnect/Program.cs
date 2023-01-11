6f75b89dcdca8af5e97fd40171888fe1dbcd942c
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-01-11 21:25:26 +0300

    Добавьте файлы проекта.

---
+++ SQLConnect/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLClass sql = new SQLClass();
+            sql.OpenConnection();
+            SqlCommand commandselect = new SqlCommand("select * from Authors; select * from Books;", sql.GetConnection());
+            //sql.InsertCommand("insert into Books values (1003,'451C farengeit',400,150);");
+            SqlDataReader reader;
+            reader = commandselect.ExecuteReader();
+            do {
+                if (reader != null)
+                { 
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[2].ToString());
+                    }
+                }
+            } 
+            while ((reader = commandselect.ExecuteReader()) != null);
+            reader.Close();
+            //sql.InsertCommand("insert into Books values (1003,'451C farengeit',400,150);");
+            //command.ExecuteReader();
+            sql.CloseConnection();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SQLConnect/SQLClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLConnect
+{
+    internal class SQLClass
+    {
+        SqlConnection connection = null;
+        public SQLClass()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Library;Integrated Security = True;";
+            connection = new SqlConnection(connectionString);
+        }
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+        public void InsertCommand(string commandString)
+        {
+            SqlCommand command = new SqlCommand(commandString, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
